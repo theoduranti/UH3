950086e25106fe10740a66bf4567a0db985e128c
Théo Duranti <theoduranti@hotmail.fr>
2018-06-23 17:14:03 +0200

    ajout de asubscribe << 0 dans create, et if dans show

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -53,6 +53,7 @@ class EventsController < ApplicationController
       @event.naturecreateur = "professeur"
       @event.professeur = "present"
       @event.professor_id = current_pro.id
+      @event.asubscribe << 0
     else
     end
 
