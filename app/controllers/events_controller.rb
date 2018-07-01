da308d7e9d06fac1880d572d70dce70fec4c7813
Théo Duranti <theoduranti@hotmail.fr>
2018-07-01 16:11:14 +0200

    search dans vue index de events et controller et model

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -5,7 +5,7 @@ class EventsController < ApplicationController
   # GET /events.json
   def index
     @events = Event.all
-    @events = Event.where(["name LIKE ? or description LIKE ? or discipline LIKE ?","%#{params[:search]}%","%#{params[:search]}%","%#{params[:search]}%"])
+    @events = Event.search(params[:search])
   end
 
   # GET /events/1
--- app/models/event.rb
+++ app/models/event.rb
@@ -7,4 +7,13 @@ class Event < ApplicationRecord
     belongs_to :professor, class_name: "Pro", optional: true
     has_and_belongs_to_many :proinvitatees, class_name: "Pro"
 
+
+    def self.search(search)
+        if search
+            where(["name LIKE ? or description LIKE ? or discipline LIKE ?","%#{search}%","%#{search}%","%#{search}%"])
+        else
+            all
+        end
+    end
+
 end
