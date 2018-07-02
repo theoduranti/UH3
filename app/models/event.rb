7b2ebbda6e516d1f62bb4200af3c6ba03862a6ca
Théo Duranti <theoduranti@hotmail.fr>
2018-07-02 22:21:02 +0200

    search5

--- app/models/event.rb
+++ app/models/event.rb
@@ -80,7 +80,7 @@ class Event < ApplicationRecord
 
     def self.search5(search5)
         if search5 == 'Les lundis'
-            where(["cast(date.strftime('%A') as text) LIKE ?", "Monday"])
+            where(["cast(date.wday as text) LIKE ?", "1"])
         else
             all
         end
