70eab617b170e86236db67b2ee68ee51b6666d63
Théo Duranti <theoduranti@hotmail.fr>
2018-07-24 21:45:57 +0200

    rotation

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,3 +14,8 @@
 //= require activestorage
 //= require turbolinks
 //= require_tree .
+
+var number = Math.floor((Math.random() * 5) + 0);
+var number2 = Math.floor((Math.random() * 0) + -5);
+$(".polaroid:nth-of-type(n+1)").css("transform", "rotate(" + number + "deg)");
+$(".polaroid:nth-of-type(n+2)").css("transform", "rotate(" + number2 + "deg)");
