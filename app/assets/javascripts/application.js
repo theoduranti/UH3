e2c24e74736f5fa6b6f3433b09f3dcdc48158767
Théo Duranti <theoduranti@hotmail.fr>
2018-07-24 23:23:08 +0200

    rotation

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -15,3 +15,9 @@
 //= require turbolinks
 //= require_tree .
 
+$('.card').hover(function() {
+    var a = Math.random() * 10 - 5;
+    $(this).css('transform', 'rotate(' + a + 'deg) scale(1.25)');
+  }, function() {
+    $(this).css('transform', 'none');
+  });
