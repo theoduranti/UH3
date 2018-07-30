8616b9ff3e287795d21b3b9681b7b15a2a7df3da
Théo Duranti <theoduranti@hotmail.fr>
2018-07-30 17:55:58 +0200

    boxprofnews:hover transform

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -34,9 +34,24 @@ $('.card').hover(function() {
 
 
   
-  function fonctiongrossimoica() {  
-    var clickcount = 0 ; 
-      if (clickcount == 0) { document.getElementsByClassName('card')[1].className="huger" ; clickcount ++ ; }  
-      else { document.getElementsByClassName('card')[1].className="anulhuger" ; clickcount -- ; }  
-      end   
-  }  
+ // function fonctiongrossimoica() {  
+ //   var clickcount = 0 ; 
+ //     if (clickcount == 0) { document.getElementsByClassName('card')[1].className="huger" ; clickcount ++ ; }  
+ //     else { document.getElementsByClassName('card')[1].className="anulhuger" ; clickcount -- ; }  
+ //     end   
+ // }  
+
+
+
+
+
+var cardsContainer = document.getElementById("eventssurordi");
+var cards = cardsContainer.getElementsByClassName("card");
+
+for (var i = 0; i < cards.length; i++) {
+  cards[i].addEventListener("click", function() {
+    var current = document.getElementsByClassName("huger");
+    current[0].className = current[0].className.replace(" huger", "");
+    this.className += " huger";
+  });
+}
